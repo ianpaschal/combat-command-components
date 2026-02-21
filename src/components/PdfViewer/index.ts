84839f7ccc9b89481cf8509b5a7497edea298273
Ian Paschal <ianpaschal@users.noreply.github.com>
2026-02-21 12:15:59 +0100

    Update index.ts

--- src/components/PdfViewer/index.ts
+++ src/components/PdfViewer/index.ts
@@ -1,3 +1,3 @@
 export { PdfViewer } from './PdfViewer';
 export { usePdfViewer } from './PdfViewer.hooks';
-export type { PdfViewerConfig, PdfViewerState } from './PdfViewer.types';
+export * from './PdfViewer.types';
