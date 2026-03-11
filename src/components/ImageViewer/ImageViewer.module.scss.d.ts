d703a07875f27c7fdc8367ccc4b086fe8c9699ab
Ian Paschal <ianpaschal@users.noreply.github.com>
2026-03-11 13:13:52 +0100

    Remove unused <ImageViewer/> style types

--- src/components/ImageViewer/ImageViewer.module.scss.d.ts
+++ src/components/ImageViewer/ImageViewer.module.scss.d.ts
@@ -1,4 +1,2 @@
 export declare const imageViewer: string;
 export declare const imageViewerImage: string;
-export declare const imageViewerImageVisible: string;
-export declare const spinnerWrapper: string;
